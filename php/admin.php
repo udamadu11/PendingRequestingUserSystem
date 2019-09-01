08568de8c6949cf071481e639e647a8a25a3a12f
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2019-09-01 23:38:38 +0530

    0.10

--- php/admin.php
+++ php/admin.php
@@ -1,5 +1,8 @@
 <?php 
 session_start();
+if ($_SESSION['login']!==true) {
+  header('location:login.php');
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -33,7 +36,17 @@ session_start();
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="mr-2" viewBox="0 0 24 24" focusable="false"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/><circle cx="12" cy="13" r="4"/></svg>
         <strong>Hi,<?php echo $_SESSION['type']; ?></strong>
       </a>
-    <a href="login.php" class="btn btn-danger">Logout</a>
+      <?php 
+        if (isset($_POST['logout'])) {
+        session_destroy();
+        header('location:admin.php');
+        }
+      ?>
+      <form method="post">
+        <button class="btn btn-danger" name="logout">Logout</button>
+      </form>
+      
+    
     </div>
   </div>
 </header>
